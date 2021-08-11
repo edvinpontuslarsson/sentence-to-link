f9005c1f35cd23bf5d4b1ebe9df1a196d1cc9dbd
edvinpontuslarsson <el223na@student.lnu.se>
2021-08-11 12:41:32 +0200

    regex for removing all but a to z and hyphen

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const clipboardy = require('clipboardy')
 
+// just option here
+console.log(process.argv)
+
+const str = 'keep-but-remove/and-also_right?'
+
+console.log(str.replace(/[^a-zA-Z- ]/g, ""))
+
+const isSpecial = process.argv[2] === 'special'
+
 const words = process.argv.splice(2)
 const link = words.map(w => w.toLowerCase()).join('-')
 
